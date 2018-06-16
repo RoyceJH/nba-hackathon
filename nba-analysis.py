7acecacb2bd8cd43fe0834d448c934150ada490e
royce <kim.royce2@gmail.com>
2018-06-16 00:04:32 -0400

    refactor out helper methods

--- nba-analysis.py
+++ nba-analysis.py
@@ -1,21 +1,11 @@
-import pandas as pd
+from helpers import convert_txt_to_df
 
-EVENT_PATH = './NBA Hackathon - Event Codes.txt'
+EVENT_CODE_PATH = './NBA Hackathon - Event Codes.txt'
 GAME_LOG_PATH = './NBA Hackathon - Game Lineup Data Sample (50 Games).txt'
 PLAY_DATA_PATH = './NBA Hackathon - Play by Play Data Sample (50 Games).txt'
 
-event_data = open(EVENT_PATH, 'r').read()
-game_log_data = open(GAME_LOG_PATH, 'r').read()
-play_data = open(PLAY_DATA_PATH, 'r').read()
 
-def setup_dataframe(data):
-    data_split = data.split('\n')
-    columns = data_split.pop(0).split('\t')
-
-    for index in range(len(data_split)):
-        data_split[index] = data_split[index].split('\t')
-    return pd.DataFrame(data_split, columns=columns)
-
-event_df = setup_dataframe(event_data)
-print(event_data.split('\n'))
-
+def generate_data():
+    event_df = convert_txt_to_df(EVENT_CODE_PATH)
+    game_log_df = convert_txt_to_df(GAME_LOG_PATH)
+    play_data_df = convert_txt_to_df(PLAY_DATA_PATH)
