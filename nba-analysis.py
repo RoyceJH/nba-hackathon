0e3dd6b147b5524bd86d3ca98a804422677286ca
royce <kim.royce2@gmail.com>
2018-06-18 12:58:21 -0400

    refactor

--- nba-analysis.py
+++ nba-analysis.py
@@ -1,11 +1,23 @@
 from helpers import convert_txt_to_df
+import pandas as pd
 
 EVENT_CODE_PATH = './NBA Hackathon - Event Codes.txt'
-GAME_LOG_PATH = './NBA Hackathon - Game Lineup Data Sample (50 Games).txt'
-PLAY_DATA_PATH = './NBA Hackathon - Play by Play Data Sample (50 Games).txt'
+GAME_LINEUP_DATA = './NBA Hackathon - Game Lineup Data Sample (50 Games).txt'
+PLAY_BY_PLAY_DATA_PATH = './NBA Hackathon - Play by Play Data Sample (50 Games).txt'
 
 
 def generate_data():
-    event_df = convert_txt_to_df(EVENT_CODE_PATH)
-    game_log_df = convert_txt_to_df(GAME_LOG_PATH)
-    play_data_df = convert_txt_to_df(PLAY_DATA_PATH)
+    event_code = convert_txt_to_df(EVENT_CODE_PATH)
+    game_lineup = convert_txt_to_df(GAME_LINEUP_DATA)
+    play_by_play = convert_txt_to_df(PLAY_BY_PLAY_DATA_PATH)
+
+    play_by_play.set_index('Game_id', inplace=True)
+    game_breakout = {}
+    print('hi')
+
+    for game_id, play in play_by_play.iterrows():
+        print((game_id, play))
+
+
+
+generate_data()
