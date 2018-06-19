7a6c7f20ef0ea1fcb34ba0a47e698444d8a9fe10
royce <kim.royce2@gmail.com>
2018-06-18 21:19:53 -0400

    breakout play data by game id

--- nba-analysis.py
+++ nba-analysis.py
@@ -5,18 +5,25 @@ EVENT_CODE_PATH = './NBA Hackathon - Event Codes.txt'
 GAME_LINEUP_DATA = './NBA Hackathon - Game Lineup Data Sample (50 Games).txt'
 PLAY_BY_PLAY_DATA_PATH = './NBA Hackathon - Play by Play Data Sample (50 Games).txt'
 
+SORT_ORDER = {
+    'BY': ['Period', 'PC_Time', 'WC_Time', 'Event_Num'],
+    'ASCENDING': [True, False, True, True]
+}
+
 
 def generate_data():
     event_code = convert_txt_to_df(EVENT_CODE_PATH)
     game_lineup = convert_txt_to_df(GAME_LINEUP_DATA)
     play_by_play = convert_txt_to_df(PLAY_BY_PLAY_DATA_PATH)
 
-    play_by_play.set_index('Game_id', inplace=True)
-    game_breakout = {}
-    print('hi')
+    all_games = play_by_play['Game_id'].unique()
+    game_breakout_df = {}
 
-    for game_id, play in play_by_play.iterrows():
-        print((game_id, play))
+    for game in all_games:
+        if game:
+            single_game_df = play_by_play.loc[play_by_play['Game_id'] == game]
+            game_breakout_df[game] = single_game_df.sort_values(by=SORT_ORDER['BY'], ascending=SORT_ORDER['ASCENDING'])
+
 
 
 
