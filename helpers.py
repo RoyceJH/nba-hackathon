accef4e602eb457fc91a8779e5acd0f7063e15f3
royce <kim.royce2@gmail.com>
2018-06-19 21:43:58 -0400

    add convertering cols to int type

--- helpers.py
+++ helpers.py
@@ -1,17 +1,30 @@
 import pandas as pd
 
+
 def setup_dataframe(data):
     """
-    :param data: raw text file data
-    :return: dataframe with first line of data as columns
+    :param data: Raw text file data
+    :return: Dataframe with first line of data as columns
     """
-    data_list = data.split('\n')
+    # Strip to remove last empty new line
+    data_list = data.strip().split('\n')
     columns = data_list.pop(0).split('\t')
 
     for index in range(len(data_list)):
         data_list[index] = data_list[index].split('\t')
     return pd.DataFrame(data_list, columns=columns)
 
+
 def convert_txt_to_df(FILE_PATH):
     file_data = open(FILE_PATH, 'r').read()
     return setup_dataframe(file_data)
+
+
+def convert_cols_to_int(df, columns):
+    """
+    :param df: Dataframe to modify
+    :param columns: List of columns to convert to int
+    :return: Modified df
+    """
+    for column in columns:
+        df[column] = df[column].astype(int)
