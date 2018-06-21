95a3bf899f2153f3a8f939e049054da473d2e1d2
royce <kim.royce2@gmail.com>
2018-06-20 22:02:51 -0400

    setup iteration of game data

--- nba-analysis.py
+++ nba-analysis.py
@@ -20,7 +20,9 @@ def generate_data():
     game_lineup = convert_txt_to_df(GAME_LINEUP_DATA)
     play_by_play = convert_txt_to_df(PLAY_BY_PLAY_DATA_PATH)
 
+    convert_cols_to_int(event_code, NUMERICAL_COLUMNS)
     convert_cols_to_int(play_by_play, NUMERICAL_COLUMNS)
+
 
     all_games = play_by_play['Game_id'].unique()
 
@@ -29,11 +31,25 @@ def generate_data():
             single_game = play_by_play.loc[play_by_play['Game_id'] == game]
             # Sorting purely based on WC_Time might have the same output
             single_game = single_game.sort_values(SORT_ORDER['BY'], ascending=SORT_ORDER['ASCENDING'])
-            player_contributions = calculate_adv_stats(single_game)
+            player_contributions = calculate_adv_stats(single_game, event_code, game_lineup)
 
 
-def calculate_adv_stats(game):
-    print('hi')
+def calculate_adv_stats(game, event_code, game_lineup):
+    player_breakdown = {}
+    on_court_players = {}
+
+    for _, play in game.iterrows():
+        _, _, event_description, action_description = lookup_event(event_code, play)
+
+        print('hi')
+
+
+
+def lookup_event(event_code, play):
+    event = event_code.loc[
+        (event_code['"Event_Msg_Type"'] == play['Event_Msg_Type']) & (event_code['"Action_Type"'] == play['Action_Type'])
+    ]
+    return event.values.flatten()
 
 
 
