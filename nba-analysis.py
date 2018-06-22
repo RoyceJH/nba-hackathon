0f0a33533cd66ada11b8ef82919daa15c548ea00
royce <kim.royce2@gmail.com>
2018-06-21 20:46:20 -0400

    add initial calculation for shots made

--- nba-analysis.py
+++ nba-analysis.py
@@ -22,6 +22,7 @@ def generate_data():
 
     convert_cols_to_int(event_code, NUMERICAL_COLUMNS)
     convert_cols_to_int(play_by_play, NUMERICAL_COLUMNS)
+    convert_cols_to_int(game_lineup, NUMERICAL_COLUMNS)
 
 
     all_games = play_by_play['Game_id'].unique()
@@ -37,12 +38,60 @@ def generate_data():
 def calculate_adv_stats(game, event_code, game_lineup):
     player_breakdown = {}
     on_court_players = {}
+    game_id = game['Game_id'][0]
+    halt_player_change = False
+    delayed_substitution = {}
+
+    # get_start_period_players(on_court_players, game_lineup, game['Game_id'][0], 1)
 
     for _, play in game.iterrows():
         _, _, event_description, action_description = lookup_event(event_code, play)
 
+        if 'Start Period' in event_description:
+            get_start_period_players(on_court_players, game_lineup, game_id, play['Period'])
+        elif 'Foul' in event_description:
+            halt_player_change = True
+        elif 'Made Shot' in event_description:
+            points = play['Option1']
+            scoring_team = play['Team_id']
+            stats_to_update = calculate_stats_to_update(on_court_players, points, scoring_team)
+            update_stats(player_breakdown, stats_to_update)
+
+def update_stats(player_breakdown, stats_to_update):
+    for player, points in stats_to_update.items():
+        try:
+            player_breakdown[player]
+        except:
+            player_breakdown[player] = 0
+
+        player_breakdown[player] += points
         print('hi')
 
+
+def calculate_stats_to_update(on_court_players, points, scoring_team):
+    stat_update = {}
+
+    for team in on_court_players:
+        if team == scoring_team:
+            for player in on_court_players[team]:
+                stat_update[player] = points
+        else:
+            for player in on_court_players[team]:
+                stat_update[player] = -points
+
+    return stat_update
+
+
+
+def get_start_period_players(on_court_players, game_lineup, game_id, period):
+    period_starting_lineup = game_lineup.loc[
+        (game_lineup['Game_id'] == game_id) & (game_lineup['Period'] == period)
+    ]
+    for _, player in period_starting_lineup.iterrows():
+        try:
+            on_court_players[player['Team_id']].append(player['Person_id'])
+        except:
+            on_court_players[player['Team_id']] = [player['Person_id']]
 
 
 def lookup_event(event_code, play):
