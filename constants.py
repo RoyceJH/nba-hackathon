28dc091b36066e101a778f99f5a88e7711ce40c9
royce <kim.royce2@gmail.com>
2018-08-06 22:45:14 -0400

    refactor variables

---
+++ constants.py
@@ -0,0 +1,16 @@
+FOUL = 'Foul'
+MADE_SHOT = 'Made Shot'
+SHOT_ATTEMPT = 'Shot'
+START_PERIOD = 'Start Period'
+SUBSTITUTION = 'Substitution'
+FREE_THROW_TECHNICAL = 'Free Throw Technical'
+FREE_THROW = 'Free Throw'
+
+SORT_ORDER = {
+    'BY': ['Period', 'PC_Time', 'WC_Time', 'Event_Num'],
+    'ASCENDING': [True, False, True, True]
+}
+NUMERICAL_COLUMNS = [
+    'Period', 'PC_Time', 'WC_Time', 'Event_Num', 'Event_Msg_Type', 'Action_Type', 'Option1', 'Option2', 'Option3',
+    'Team_id_type'
+]
